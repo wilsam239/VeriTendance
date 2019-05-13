24c5ff4eb8f55700190fd273f5ab784c788dd3b6
wilsam239 <swilliamson239@hotmail.com>
2019-05-13 22:51:33 +1000

    Initial Commit
    App has been setup in Android Studio with 4 navigation buttons.

---
+++ app/src/main/java/com/example/veritendance/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.veritendance;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_employees:
+                    mTextMessage.setText(R.string.title_employees);
+                    return true;
+                case R.id.navigation_history:
+                    mTextMessage.setText(R.string.title_history);
+                    return true;
+                case R.id.navigation_session:
+                    mTextMessage.setText(R.string.title_session);
+                    return true;
+                case R.id.navigation_topics:
+                    mTextMessage.setText(R.string.title_topics);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
