862d21c149ebdeb04539b9633d582598a8fc1f5d
wilsam239 <swilliamson239@hotmail.com>
2019-05-17 17:26:58 +1000

    Added new Fragments
    Fragments added for employees, history, new session and topics. Attempted to replace fragment container.

--- app/src/main/java/com/example/veritendance/MainActivity.java
+++ app/src/main/java/com/example/veritendance/MainActivity.java
@@ -5,13 +5,18 @@ import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Begin the transaction
+    private FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
     private TextView mTextMessage;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -21,16 +26,27 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_employees:
                     mTextMessage.setText(R.string.title_employees);
+                    ft.replace(R.id.fragment_container, new employeeFragment());
+                    // Complete the changes added above
+                    ft.commit();
                     return true;
                 case R.id.navigation_history:
                     mTextMessage.setText(R.string.title_history);
-
+                    ft.replace(R.id.fragment_container, new historyFragment());
+                    // Complete the changes added above
+                    ft.commit();
                     return true;
                 case R.id.navigation_session:
                     mTextMessage.setText(R.string.title_session);
+                    ft.replace(R.id.fragment_container, new newSessionFragment());
+                    // Complete the changes added above
+                    ft.commit();
                     return true;
                 case R.id.navigation_topics:
                     mTextMessage.setText(R.string.title_topics);
+                    ft.replace(R.id.fragment_container, new topicsFragment());
+                    // Complete the changes added above
+                    ft.commit();
                     return true;
             }
             return false;
@@ -45,6 +61,10 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        // Replace the contents of the container with the new fragment
+        ft.replace(R.id.fragment_container, new employeeFragment());
+        // Complete the changes added above
+        ft.commit();
     }
 
 }
---
+++ app/src/main/java/com/example/veritendance/newSessionFragment.java
@@ -0,0 +1,18 @@
+package com.example.veritendance;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class newSessionFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_new_session, parent, false);
+    }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        
+    }
+}
